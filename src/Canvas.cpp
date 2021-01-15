f6189ba31996fa8dffb6607a664c5cc9bb75de49
Andrew Jouffray <andrewjouffray@hotmail.fr>
2021-01-15 03:14:01 -0700

    renamed the properties in the canvas.cpp class to match with the .h file

--- src/Canvas.cpp
+++ src/Canvas.cpp
@@ -3,14 +3,14 @@
 author: Andrew Jouffray
 date: jan 2021
 
-A Canvas is the image that the neural network will end up seeing, millions of unique canvases can be generated if needed. Each canvas is a backround image, 
-and between 2 and 10 object of interest copied on top of the background image, each canvas object has a mask where a detailed outline of the objects in interest in the image 
+A Canvas is the image that the neural network will end up seeing, millions of unique Canvas::canvases can be generated if needed. Each Canvas::canvas is a backround image, 
+and between 2 and 10 object of interest copied on top of the Canvas::background image, each Canvas::canvas object has a Canvas::mask where a detailed outline of the Canvas::objects in interest in the image 
 are available for the model to learn, Bounding boxes are also available to the model.
 
-step1: define the size and aspect ratio of the canvas 
-step2: create the Ooi and copy them on top of the canvas
-step3: create a mask of the canvas
-step4: get the Bounding box coordinates of each object in the canvas
+step1: define the size and aspect ratio of the Canvas::canvas 
+step2: create the Ooi and copy them on top of the Canvas::canvas
+step3: create a Canvas::mask of the Canvas::canvas
+step4: get the Bounding box coordinates of each object in the Canvas::canvas
 step5: replace the bacl pixels with the provided backgorund image
 step6: randomely apply transforamtions: (resolution change, blurr, brightness change)
 
@@ -19,62 +19,62 @@ step6: randomely apply transforamtions: (resolution change, blurr, brightness ch
 
 
 // constructor, params need to be added I think.
-Canvas::Canvas(cv::Mat ooiArg, cv::Mat backgroundArg, int maxOoi, int[2] modProbability, bool debug){
+Canvas::Canvas(cv::Mat ooiArg, cv::Mat Canvas::backgroundArg, int maxOoi, int[2] modProbability, bool Canvas::debug ){
 
-	ooi = ooiArg;
-	background = backgroundArg;
-	maxObjects = maxOoi
+	Canvas::ooi = ooiArg;
+	Canvas::background = Canvas::backgroundArg;
+	Canvas::maxObjects = maxOoi
 
-	// probablity of modifying the Oois not the whole canvas			
-	modProb = modProbability;
+	// probablity of modifying the Oois not the whole Canvas::canvas			
+	Canvas::modProb = modProbability;
 
-	// used to randomely apply transformations to the canvas
-	canvasModProb = randomInt(1, 100);
+	// used to randomely apply transformations to the Canvas::canvas
+	Canvas::canvasModProb = randomInt(1, 100);
 
 	// define the size random between 500 and 720 pixels
-	height = randomInt(500, 860);
+	Canvas::height = randomInt(500, 860);
 
 	// gets a aspect random ratio from the ratio list
 	int randRatio = randomInt(0, 4);
-	float ratio = aspectRatios[randRatio];	
-	float fWidth = height * ratio;
-	width = (int)fWidth; // gets a with value as integer
+	float ratio = Canvas::aspectRatios[randRatio];	
+	float fWidth = Canvas::height * ratio;
+	Canvas::width = (int)fWidth; // gets a with value as integer
 
-	// create canvas and set it to black canvas to black
+	// create Canvas::canvas and set it to black Canvas::canvas to black
 	cv::Mat blkImage(320, 240, CV_8UC3, cv::Scalar(0, 0, 0));
-	canvas = blkImage;
+	Canvas::canvas = blkImage;
 
-	//define the number of Ooi to be created for this canvas
-	numObjects = randomInt(2, maxObjects);
+	//define the number of Ooi to be created for this Canvas::canvas
+	Canvas::numObjects = randomInt(2, Canvas::maxObjects);
 
-	// one object per column to avoid overlapping of objects on the image
-	columnWidth = width / numObjects;
+	// one object per column to avoid overlapping of Canvas::objects on the image
+	Canvas::columnWidth = Canvas::width / Canvas::numObjects;
 
 	createCanvas();
 
-	// Creates a mask of the silouette of the object of interest in a color unique 
+	// Creates a Canvas::mask of the silouette of the object of interest in a color unique 
 	// to the label 
 	int testColor [3] = {210, 30, 78}; // make sure to properly implement this.
 	createMasks(testColor);
 
 
-	// Calculates an accurate position of all the objects of interest to draw bonding boxes arund them
-	rois = calculateRois();
+	// Calculates an accurate position of all the Canvas::objects of interest to draw bonding boxes arund them
+	Canvas::rois = calculateRois();
 				
 				
-	// replaces black with a background image
+	// replaces black with a Canvas::background image
 	addBackground();
 
 	// transformations
-	if (canvasModProb % 2 == 0){
+	if (Canvas::canvasModProb % 2 == 0){
 		changeBrightness();
 	}
 
-	if (canvasModProd > 75){
+	if (Canvas::canvasModProd > 75){
 		blurr();
 	}
 
-	if (canvasModProd % 5 == 0){
+	if (Canvas::canvasModProd % 5 == 0){
 		lowerRes();
 	}
 
@@ -85,12 +85,12 @@ Canvas::Canvas(cv::Mat ooiArg, cv::Mat backgroundArg, int maxOoi, int[2] modProb
 // generates the image
 void Canvas::createCanvas(){
 
-	for(int i = 0; i < numObjects; i ++){
+	for(int i = 0; i < Canvas::numObjects; i ++){
 
-		int absolutePos = columnWidth * i;
+		int absolutePos = Canvas::columnWidth * i;
 
-		// create an object to put on the canvas
-		Ooi objectOfInterest = Ooi(ooi, columnWidth, height, absolutePos, modProb, debug);
+		// create an object to put on the Canvas::canvas
+		Ooi objectOfInterest = Ooi(ooi, Canvas::columnWidth, Canvas::height, absolutePos, Canvas::modProb, Canvas::debug );
 		objects.push_back(objectOfInterest);
 
 		// get the position of the object in the image
@@ -100,24 +100,24 @@ void Canvas::createCanvas(){
 		int x2 = positions[2];
 		int y2 = positions[3];
 
-		// overlay the object onto the canvas 
+		// overlay the object onto the Canvas::canvas 
 		try{
 			cv::Mat objectImage = objectOfInterest.getObject();
-			cv::Mat inset(canvas, cv::Rect(x1, y1, x2, y2));
+			cv::Mat inset(Canvas::canvas, cv::Rect(x1, y1, x2, y2));
 			objectImage.copyTo(inset);
 				
 		}catch{
 
 			// maybe add a counter for failures?
-			if(debug) {
+			if(Canvas::debug ) {
 					
-				cout << "> (canvas) could not insert object into canvas" << endl;
-				cout << "> (canvas) x1, y1, x2, y2: "+x1+", "+y1+", "+x2+", "+y2 << endl;
-				cout << "> (canvas) canvas size, rows, cols: " +canvas.rows+", "+canvas.cols;
+				cout << "> (Canvas::canvas) could not insert object into Canvas::canvas" << endl;
+				cout << "> (Canvas::canvas) x1, y1, x2, y2: "+x1+", "+y1+", "+x2+", "+y2 << endl;
+				cout << "> (Canvas::canvas) Canvas::canvas size, rows, cols: " +Canvas::canvas.rows+", "+Canvas::canvas.cols;
 			}
 			else{
 						
-				cout << "> (canvas) could not insert object into canvas";
+				cout << "> (Canvas::canvas) could not insert object into Canvas::canvas";
 			}
 				
 		}	
@@ -136,20 +136,20 @@ void Canvas::lowerRes(){
 	float factor = 0.0;
 
 	//allows the resolution do be reduced more on larger images
-	if (height > 720){
+	if (Canvas::height > 720){
 		factor = randomFloat(1.0, 2.0);
 	}else{
 		factor = randomFloat(1.0, 1.5);
 	}
 
-	float newHeightf = height/factor;
-	float newWidthf = width/factor;
+	float newHeightf = Canvas::height/factor;
+	float newWidthf = Canvas::width/factor;
 	int newWidth = (int)newWidthf;
 	int newHeight = (int)newHeightf;
 
 	cv::Mat lowRes;
-	cv::resize(canvas, lowRes, cv::(newWidth, newHeight));
-	cv::resize(lowRes, canvas, cv::(width, height));
+	cv::resize(Canvas::canvas, lowRes, cv::(newWidth, newHeight));
+	cv::resize(lowRes, Canvas::canvas, cv::(Canvas::width, Canvas::height));
 
 }
 
@@ -161,39 +161,39 @@ void Canvas::blurr(){
 		kernelSize = kernelSize + 1;
 	}
 		
-	cv::GaussianBlur(canvas, canvas, cv::Size(kernelSize, kernelSize), 0);
+	cv::GaussianBlur(Canvas::canvas, Canvas::canvas, cv::Size(kernelSize, kernelSize), 0);
 
 }
 
-// each label in a dataset has a different colored mask
+// each label in a dataset has a different colored Canvas::mask
 void Canvas::createMasks(int mcolors [3]){
 
 	// FUTURE UPDATE: get the thresholded value once and not in each method
 
-	mask = canvas.clone()
-        cv::cvtColor(mask, mask, cv::COLOR_BGR2GRAY);
+	Canvas::mask = Canvas::canvas.clone()
+        cv::cvtColor(Canvas::mask, Canvas::mask, cv::COLOR_BGR2GRAY);
         double thresh = 0;
         double maxValue = 255;
         // Binary Threshold
-        cv::threshold(mask, mask, thresh, maxValue, cv::THRESH_BINARY);
+        cv::threshold(Canvas::mask, Canvas::mask, thresh, maxValue, cv::THRESH_BINARY);
         cv::Mat grayMask;
-        cv::cvtColor(mask, mask, cv::COLOR_GRAY2BGR);
+        cv::cvtColor(Canvas::mask, Canvas::mask, cv::COLOR_GRAY2BGR);
 
-	// assing the black mask used in other methods
-	blackMask = maks.clone();
+	// assing the black Canvas::mask used in other methods
+	Canvas::blackMask = maks.clone();
 
-        inRange(mask, cv::Scalar(255, 255, 255), cv::Scalar(255, 255, 255), grayMask);
-        mask.setTo(cv::Scalar(mcolors[0], mcolors[1], mcolors[2]), grayMask);
+        inRange(Canvas::mask, cv::Scalar(255, 255, 255), cv::Scalar(255, 255, 255), grayMask);
+        Canvas::mask.setTo(cv::Scalar(mcolors[0], mcolors[1], mcolors[2]), grayMask);
 
 }
 
 vector<cv::rect> Canvas::calculateRois(){
 			
-	// if this breaks it might be due to black mask being in brg and not in gray
+	// if this breaks it might be due to black Canvas::mask being in brg and not in gray
 				
 	int thresh = 100;
         cv::Mat canny_output;
-        cv::Canny(blackMask, canny_output, thresh, thresh*2);
+        cv::Canny(Canvas::blackMask, canny_output, thresh, thresh*2);
 
         vector<vector<cv::Point>> contours;
         cv::findContours(canny_output, contours, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
@@ -216,43 +216,43 @@ vector<cv::rect> Canvas::calculateRois(){
                	}
 	}
 
-	// each rectangle is define like this [x,y,width,height]
+	// each rectangle is define like this [x,y,Canvas::width,Canvas::height]
 	// Point point0 = Point(rectangle.x, rectangle.y);
 	return boundRect;
 
 
 }
 
-// adds a background image to the black background of the canvas
+// adds a Canvas::background image to the black Canvas::background of the Canvas::canvas
 cv::Mat Canvas::addBackground(){
 	
-        // resize the background to fit the canvas
-        cv::resize(background, background, cv::Size(blackMask.cols, blackMask.rows));
+        // resize the Canvas::background to fit the Canvas::canvas
+        cv::resize(Canvas::background, Canvas::background, cv::Size(Canvas::blackMask.cols, Canvas::blackMask.rows));
 
 	// all balck pixels become the bacjgorund image
-        cv::Mat comb = blackMask + background;
+        cv::Mat comb = Canvas::blackMask + Canvas::background;
 
 	// all remaining white pixels become black
         comb.setTo(0, comb == 255); // comb means combined images
 
-	// all black pixels are set to canvas
-        cv::Mat comb2 =  comb + canvas;
+	// all black pixels are set to Canvas::canvas
+        cv::Mat comb2 =  comb + Canvas::canvas;
         		
-	canvas = comb2;
+	Canvas::canvas = comb2;
 	
 }
 
 void Canvas::ChangeBrightness(){
 
 	// HSV stands for Hue Saturation Value(Brightness)
-       	cv::cvtColor(Ooi::canvas,Ooi::canvas,CV_BGR2HSV);
+       	cv::cvtColor(Ooi::Canvas::canvas,Ooi::Canvas::canvas,CV_BGR2HSV);
 
        	// hsv values are on a scale from 0 to 255
         int defaultVal = 255;
 
         int randValue = randomInt(155, 355);
 
-        // value is brightness, we modify this on the whole canvas not on one object, we don't modify it here.
+        // value is brightness, we modify this on the whole Canvas::canvas not on one object, we don't modify it here.
         int value = randValue - defaultVal;
 
         // iterate over the Ooi::image
@@ -274,19 +274,19 @@ void Canvas::ChangeBrightness(){
                 }
         }
 
-        cv::cvtColor(canvas,canvas,cv::CV_HSV2BGR);
+        cv::cvtColor(Canvas::canvas,Canvas::canvas,cv::CV_HSV2BGR);
 
 
 }
 
 cv::Mat Canvas::getCanvas(){
 		
-	return canvas;
+	return Canvas::canvas;
 }
 
 cv::Mat Canvas::getMask(){
 		
-	return mask;
+	return Canvas::mask;
 }
 
 vector<vector<int>> Canvas::getRois(){
