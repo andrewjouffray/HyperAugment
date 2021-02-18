3388ecd9c58a70f312028b4b49241e2357cc3621
Andrew Jouffray <andrewjouffray@hotmail.fr>
2021-02-17 17:20:48 -0700

    got tinyxml working in a test

--- unit-tests/xml-test/test.cpp
+++ unit-tests/xml-test/test.cpp
@@ -5,176 +5,115 @@
 #include <string>
 #include <filesystem>
 #include <chrono>
-#include "randomFunc.h"
 #include <sys/stat.h>
+#include "./libs/tinyxml/tinyxml.h"
+#include "./libs/tinyxml/tinystr.h"
 
-// https://en.cppreference.com/w/cpp/filesystem/create_directory
-// https://stackoverflow.com/questions/6926433/how-to-shuffle-a-stdvector
+//https://stackoverflow.com/questions/701648/create-xml-files-from-c-program
 
 namespace fs = std::filesystem;
 using namespace std;
 
-// checks if a directory exists
-int dirExists(const char* const path)
+void build_simple_doc( )
 {
-    struct stat info;
 
-    int statRC = stat( path, &info );
-    if( statRC != 0 )
-    {
-        if (errno == ENOENT)  { return 0; } // something along the path does not exist
-        if (errno == ENOTDIR) { return 0; } // something in path prefix is not a dir
-        return -1;
-    }
+	// dummy vectors
+	vector<int> roi1 = vect(4, 10);
+	vector<int> roi2 = vect(4, 6);
+	vector<int> roi3 = vect(4, 23);
 
-    return ( info.st_mode & S_IFDIR ) ? 1 : 0;
-}
+	// dummy vector in the format of the getRois method
+	vector<vector<int>> rois;
+	rois.push_back(roi1);
+	rois.push_back(roi2);
+	rois.push_back(roi3);
 
-// saves all the names of the files in a given path
-vector<string> getFiles(string path){
-
-        //cout << "adding " + path << endl;
-        // this is it for now
-        vector<string> files;
-
-        for(const auto & entry : fs::directory_iterator(path)){
-                string it = entry.path();
-
-                //cout << it << endl;
-                files.push_back(it);
-        }
-
-        return files;
-
-
-}
-
-bool createOutputDirs(string outputPath){
-
-	// three directories to save all the augmented data
-	string masks = outputPath + "masks/";
-	string imgs = outputPath + "imgs/";
-	string xml = outputPath + "xml/"; 
+	TiXmlDocument doc;
+	TiXmlDeclaration * decl = new TiXmlDeclaration( "1.0", "", "" );
+	TiXmlElement * annotation = new TiXmlElement( "annotation" );
+	TiXmlElement * folder = new TiXmlElement( "folder" );
+	TiXmlElement * filename = new TiXmlElement( "filename" );
+	TiXmlElement * path = new TiXmlElement( "path" );
+	TiXmlElement * source = new TiXmlElement( "source" );
+	TiXmlElement * database = new TiXmlElement( "database" );
+	TiXmlElement * size = new TiXmlElement( "size" );
+	TiXmlElement * width = new TiXmlElement( "width" );
+	TiXmlElement * height = new TiXmlElement( "height" );
+	TiXmlElement * depth = new TiXmlElement( "depth" );
+	TiXmlElement * segmented = new TiXmlElement( "segmented" );
+	TiXmlElement * object = new TiXmlElement( "object" );
+	TiXmlElement * name = new TiXmlElement( "name" );
+	TiXmlElement * pose = new TiXmlElement( "pose" );
+	TiXmlElement * truncated = new TiXmlElement( "truncated" );
+	TiXmlElement * difficult = new TiXmlElement( "difficult" );
+	TiXmlElement * bndbox = new TiXmlElement( "bndbox" );
+	TiXmlElement * xmin = new TiXmlElement( "xmin" );
+	TiXmlElement * ymin = new TiXmlElement( "ymin" );
+	TiXmlElement * xmax = new TiXmlElement( "xmax" );
+	TiXmlElement * ymax = new TiXmlElement( "ymax" );
 
 
+	TiXmlText * folderName = new TiXmlText( "masks-test" );
+	TiXmlText * fileNameText = new TiXmlText( "fileName.jpg" );
+	TiXmlText * databaseName = new TiXmlText( "Unknown" );
+	TiXmlText * pathText = new TiXmlText( "/home/andrew/this/that/here/there" );
+	TiXmlText * widthVal = new TiXmlText( "736" );
+	TiXmlText * heightVal = new TiXmlText( "736" );
+	TiXmlText * depthVal = new TiXmlText( "3" );
+	TiXmlText * segmentedVal = new TiXmlText( "0" );
+	TiXmlText * objectName = new TiXmlText( "person" );
+	TiXmlText * objectPose = new TiXmlText( "Unspecified" );
+	TiXmlText * objectTruncated = new TiXmlText( "0" );
+	TiXmlText * objectDifficult = new TiXmlText( "0" );
 
-	if (!dirExists(outputPath.c_str())){
+
+	annotation->LinkEndChild( folder );
+		folder->LinkEndChild(folderName);
+
+	annotation->LinkEndChild( filename );
+		filename->LinkEndChild(fileNameText);
+
+	annotation->LinkEndChild( path );
+		path->LinkEndChild(pathText);
+
+	annotation->LinkEndChild( source );
+		source->LinkEndChild( database );
+			database->LinkEndChild( databaseName );
+
+	annotation->LinkEndChild( size );
+		size->LinkEndChild( width );	
+			width->LinkEndChild( widthVal );
+		size->LinkEndChild( height );
+			height->LinkEndChild( heightVal );
+		size->LinkEndChild( depth );	
+			depth->LinkEndChild( depthVal );
+
+	annotation->LinkEndChild( segmented );
+		segmented->LinkEndChild( segmentedVal );
+
+	// add all the objects here might need to create them in the loop
+	for(vector<int> roi : rois){
 	
-		cout << "> Label: " << outputPath << " does not exist." << endl;
-
-		if(fs::create_directory(outputPath)){
-			cout << "> Label: created " << outputPath << endl;
-		}else{
-		
-			cout << "> Label: could not create " << outputPath;
-			throw "Error: Could not create output path.";
-		}
-	
+		annotation->LinkEndChild( object );
+			object->LinkEndChild(name);
+				name->LinkEndChild(objectName);
+			object->LinkEndChild(pose);
+				pose->LinkEndChild(objectPose);
+			object->LinkEndChild(truncated);
+				truncated->LinkEndChild(objectTruncated);
+			object->LinkEndChild()
 	}
 
-	if(dirExists(masks.c_str())){
-		cout << "> Output for masks/ already exists, using existing directory" << endl;
-	}else{
-
-		if(fs::create_directory(masks)){
-			cout << "> Label: created " << masks << endl;
-		}else{
-		
-			cout << "> Label: could not create " << masks << endl;
-			throw "Error: Could not create masks path.";
-		}
-
-	}
-
-	if(dirExists(imgs.c_str())){
-		cout << "> Output for imgs/ already exists, using existing directory" << endl;
-	}else{
-
-		if(fs::create_directory(imgs)){
-			cout << "> Label: created " << imgs << endl;
-		}else{
-		
-			cout << "> Label: could not create " << imgs << endl;
-			throw "Error: Could not create imgs path.";
-		}
-
-	}
-
-	if(dirExists(xml.c_str())){
-		cout << "> Output for xml/ already exists, using existing directory" << endl;
-	}else{
-
-		if(fs::create_directory(xml)){
-			cout << "> Label: created " << xml << endl;
-		}else{
-		
-			cout << "> Label: could not create " << xml << endl;
-			throw "Error: Could not create xml path.";
-		}
-
-	}
-	
-	return true;
 
 
-
+	doc.LinkEndChild( decl );
+	doc.LinkEndChild( annotation );
+	doc.SaveFile( "labelImgDemo.xml" );
 }
-
-vector<string> getInputs(string inputPath){
-
-	vector<string> inputs;
-
-	if (dirExists(inputPath.c_str())){
-       		inputs = getFiles(inputPath);
-       	}else{
-                cout << "> Label: error could not find path " << inputPath << endl;
-                throw "> Error: Invalid input path.";
-        }
-
-	return inputs;
-}
-
-vector<string> getBackgrounds(string backgroundPath){
-
-	vector<string> backgrounds;
-
-	if (dirExists(backgroundPath.c_str())){
-       		backgrounds = getFiles(backgroundPath);
-       	}else{
-                cout << "> Label: error could not find path " << backgroundPath << endl;
-                throw "> Error: Invalid background image path.";
-        }
-
-	// shuffle the background images around 
-	auto rng = default_random_engine {};
-	shuffle(begin(backgrounds), end(backgrounds), rng);
-
-	return backgrounds;
-
-
-}
-
 
 int main(int argc, char** argv){
 
-	vector<string> inputs;
-	vector<string> backgrounds;
-	string inputPath = "/home/andrew/Projects/School/os/";
-	string outputPath = "/home/andrew/Projects/Projects/data/test/";
-	string backgroundPath = "/home/andrew/Programs/Tensorflow/augmentation/randomShapes/blurry-backgrounds";
-
-	inputs = getInputs(inputPath);
-	cout << inputs.size() << endl;
-
-	backgrounds = getBackgrounds(backgroundPath);
-	cout << backgrounds.size() << endl;
-
-	for (int i = 0; i < 35; i ++){
-		cout << backgrounds.at(i) <<endl;
-	}
-
-	bool worked = createOutputDirs(outputPath);
-
+	build_simple_doc();
 
 	return 0;
 }
