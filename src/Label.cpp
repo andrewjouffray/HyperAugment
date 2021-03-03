d30c46f099b08c02ab7dc3b1c96a138af2b73a4d
Andrew Jouffray <andrewjouffray@hotmail.fr>
2021-03-02 21:44:09 -0700

    started integrading the xml generation into label.cpp

--- src/Label.cpp
+++ src/Label.cpp
@@ -31,6 +31,9 @@ https://stackoverflow.com/questions/18100097/portable-way-to-check-if-directory-
 #include "randomFunc.h"
 #include "Canvas.h"
 #include <sys/stat.h>
+#include "./vendors/tinyxml/tinyxml.h"
+#include "./vendors/tinyxml/tinystr.h"
+
 
 namespace fs = std::filesystem;
 using namespace std;
@@ -82,8 +85,125 @@ void saveMask(cv::Mat mask, string name){
 
 }
 
-void saveXML(vector<vector<int>> rois, string name){
+void saveXML(vector<vector<int>> rois, string name, string folder, string path, cv::Mat img){
 
+	int height = img.rows;
+	int width = img.cols;
+
+	string sHeight = to_string(height);
+       	string sWidth = to_string(width);
+
+	string fullPath = outputPath + xml;	
+
+	TiXmlDocument doc;
+        TiXmlDeclaration * decl = new TiXmlDeclaration( "1.0", "", "" );
+        TiXmlElement * annotation = new TiXmlElement( "annotation" );
+        TiXmlElement * folder = new TiXmlElement( "folder" );
+        TiXmlElement * filename = new TiXmlElement( "filename" );
+        TiXmlElement * path = new TiXmlElement( "path" );
+        TiXmlElement * source = new TiXmlElement( "source" );
+        TiXmlElement * database = new TiXmlElement( "database" );
+        TiXmlElement * size = new TiXmlElement( "size" );
+        TiXmlElement * width = new TiXmlElement( "width" );
+        TiXmlElement * height = new TiXmlElement( "height" );
+        TiXmlElement * depth = new TiXmlElement( "depth" );
+        TiXmlElement * segmented = new TiXmlElement( "segmented" );
+
+
+        TiXmlText * folderName = new TiXmlText( "masks-test" );
+        TiXmlText * fileNameText = new TiXmlText( "fileName.jpg" );
+        TiXmlText * databaseName = new TiXmlText( "Unknown" );
+        TiXmlText * pathText = new TiXmlText( fullPath.c_str() );
+        TiXmlText * widthVal = new TiXmlText( sWidth.c_str() );
+        TiXmlText * heightVal = new TiXmlText( sHeight.c_str() );
+        TiXmlText * depthVal = new TiXmlText( "3" );
+        TiXmlText * segmentedVal = new TiXmlText( "0" );
+
+
+        annotation->LinkEndChild( folder );
+                folder->LinkEndChild(folderName);
+
+        annotation->LinkEndChild( filename );
+                filename->LinkEndChild(fileNameText);
+
+        annotation->LinkEndChild( path );
+                path->LinkEndChild(pathText);
+
+        annotation->LinkEndChild( source );
+                source->LinkEndChild( database );
+                        database->LinkEndChild( databaseName );
+
+        annotation->LinkEndChild( size );
+                size->LinkEndChild( width );
+                        width->LinkEndChild( widthVal );
+                size->LinkEndChild( height );
+                        height->LinkEndChild( heightVal );
+                size->LinkEndChild( depth );
+                        depth->LinkEndChild( depthVal );
+
+        annotation->LinkEndChild( segmented );
+                segmented->LinkEndChild( segmentedVal );
+
+        // add all the objects here might need to create them in the loop
+        for(vector<int> roi : rois){
+
+                TiXmlElement * object = new TiXmlElement( "object" );
+                TiXmlElement * name = new TiXmlElement( "name" );
+                TiXmlElement * pose = new TiXmlElement( "pose" );
+                TiXmlElement * truncated = new TiXmlElement( "truncated" );
+                TiXmlElement * difficult = new TiXmlElement( "difficult" );
+                TiXmlElement * bndbox = new TiXmlElement( "bndbox" );
+                TiXmlElement * xmin = new TiXmlElement( "xmin" );
+                TiXmlElement * ymin = new TiXmlElement( "ymin" );
+                TiXmlElement * xmax = new TiXmlElement( "xmax" );
+                TiXmlElement * ymax = new TiXmlElement( "ymax" );
+
+                TiXmlText * objectName = new TiXmlText( name.c_str() );
+                TiXmlText * objectPose = new TiXmlText( "Unspecified" );
+                TiXmlText * objectTruncated = new TiXmlText( "0" );
+                TiXmlText * objectDifficult = new TiXmlText( "0" );
+
+                string x1 = to_string(roi[0]);
+                string y1 = to_string(roi[1]);
+                string x2 = to_string(roi[2]);
+                string y2 = to_string(roi[3]);
+
+                TiXmlText * objectXmin = new TiXmlText( x1.c_str() );
+                TiXmlText * objectYmin = new TiXmlText( y1.c_str() );
+                TiXmlText * objectXmax = new TiXmlText( x2.c_str() );
+                TiXmlText * objectYmax = new TiXmlText( y2.c_str() );
+
+
+
+                annotation->LinkEndChild( object );
+                        object->LinkEndChild(name);
+                                name->LinkEndChild(objectName);
+                        object->LinkEndChild(pose);
+                                pose->LinkEndChild(objectPose);
+                        object->LinkEndChild(truncated);
+                                truncated->LinkEndChild(objectTruncated);
+                        object->LinkEndChild(difficult);
+                                difficult->LinkEndChild(objectDifficult);
+                        object->LinkEndChild(bndbox);
+                                bndbox->LinkEndChild(xmin);
+                                        xmin->LinkEndChild(objectXmin);
+                                bndbox->LinkEndChild(ymin);
+                                        ymin->LinkEndChild(objectYmin);
+                                bndbox->LinkEndChild(xmax);
+                                        xmax->LinkEndChild(objectXmax);
+                                bndbox->LinkEndChild(ymax);
+                                        ymax->LinkEndChild(objectYmax);
+
+
+        }
+
+
+
+
+
+        doc.LinkEndChild( decl );
+        doc.LinkEndChild( annotation );
+        doc.SaveFile( "labelImgDemo.xml" );
 	// need to test this
 
 }
