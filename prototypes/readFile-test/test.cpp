be34c748543fa151d190c013650b398388e24ee1
Andrew Jouffray <andrewjouffray@hotmail.fr>
2021-03-07 19:46:43 -0700

    added a string parser to the readfile prototype

--- prototypes/readFile-test/test.cpp
+++ prototypes/readFile-test/test.cpp
@@ -9,11 +9,12 @@
 namespace fs = std::filesystem;
 using namespace std;
 
-void loadFile( )
+vector<vector<string>> parseFile( )
 {
 
 	string line;
 	ifstream myfile ("../config.txt");
+	vector<vector<string>> parsedFile;
 	if (myfile.is_open()){
     		while ( getline (myfile,line) ){
       			//cout << line << '\n';
@@ -26,19 +27,15 @@ void loadFile( )
 			while ((pos = line.find(delimiter)) != std::string::npos) {
     				token = line.substr(0, pos);
 				// check if empty char
-				if (token.compare(" ") == 0){
-				
+				if (token.compare(" ") != 0 && token.compare("") != 0){
+					splitLine.push_back(token);
 				}
-				splitLine.push_back(token);
     				line.erase(0, pos + delimiter.length());
 			}
-
-			splitLine.push_back(line);
-			for(string word: splitLine){
-
-				cout << " split= " << word << endl;
-			
+			if (token.compare(" ") != 0){
+				splitLine.push_back(line);
 			}
+			parsedFile.push_back(splitLine);
     		}
    	 myfile.close();
  	
@@ -46,11 +43,52 @@ void loadFile( )
   	else{ cout << "Unable to open file";
 
 	}
-}	
+
+	return parsedFile;
+}
+
+void setSettings (vector<vector<string>> file){
+
+	int affineProb = 50;
+	int changeSatProb = 30;
+	int changeBrightProb = 30;
+	int blurrProb = 30;
+	int mult = 30;
+	int objects = 40;
+	string pathToBackground;
+	string output;
+	string label;
+	string dataset;
+
+	for(vector<string> line : file){
+		
+		cout << "\n new line: ";
+		int i = 0;
+		for(string word : line){
+		
+			if(word.compare("dataset") == 0){
+			
+				dataset = line[i+1];
+
+			}else if(word.compare("label") == 0){
+			
+				label = line[i+1];
+			}
+
+
+			i++;
+		}
+
+	}
+
+
+
+}
 
 int main(int argc, char** argv){
 
-	loadFile();
+	vector<vector<string>> file = parseFile();
+
 
 	return 0;
 }
